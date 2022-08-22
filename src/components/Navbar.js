e1b1adb0b8818bdf731277b88bf828ef175ec1e5
Francisco Rosete <fjrosete@gmail.com>
2022-08-22 00:53:17 -0700

    added navbar link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,8 +6,9 @@ const Navbar = () => {
     <nav className="flex mx-auto justify-between">
       <Link to="/" className="font-bold">TASK MANAGER</Link>
       <div>
-        <NavLink className='px-2' to='/'>Tasks</NavLink>
-        <NavLink to='/contact'>Contact</NavLink>
+        <NavLink className='px-2' to='/'>Manage Tasks</NavLink>
+        <NavLink className='px-2' to='/create-task'>Create Task</NavLink>
+        <NavLink className='px-2'to='/contact'>Contact</NavLink>
       </div>
     </nav>
   );
